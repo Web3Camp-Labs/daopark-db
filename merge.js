ab8c143e82dbfcce860f51ea590d585dd2d3a791
Frozen <1884084+xrdavies@users.noreply.github.com>
2022-03-21 14:37:44 +0800

    add merge.js

---
+++ merge.js
@@ -0,0 +1,19 @@
+import * as fs from 'fs';
+import 'dotenv/config';
+import { GithubKV } from "github-keyvalue";
+
+async function main() {
+  let db = new GithubKV({ token: process.env.PERSONAL_TOKEN, owner: 'web3-camp', repo: 'test-issue', branch: 'db' });
+  let alldaos = await db.list();
+  console.log(alldaos);
+
+  let approvedList = alldaos.fitler((item) => {return item.data.DAOStatus == 'approved'}).map((item) => { return item.data});
+  fs.writeFileSync("dao.json", JSON.stringify(approvedList, null, 2), 'utf8');
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
